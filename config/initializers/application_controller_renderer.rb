30ebdea4b4d9091c2dd674155f8ad06daa49531f
fr15ky3816 <shota.t.3815@gmail.com>
2021-04-15 15:34:58 +0000

    add 4/15

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = 'fc9333e1a69eae481a0bbe251070aeb64837c3ab05f40bcf2a6baa9fc3b4deea0caca60f058f68a57b3d7ccacf129e400188a61057456b88f89f8d9c6109210d'
