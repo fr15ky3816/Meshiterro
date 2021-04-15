8840d8ade26285890314269c544d2ea120f1a206
fr15ky3816 <shota.t.3815@gmail.com>
2021-04-15 18:04:25 +0000

    add 4/15 3rd

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get 'users/show'
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 root to: 'homes#top'
@@ -6,7 +7,7 @@ resources :post_images, only: [:new, :create, :index, :show, :destroy] do
   resource :favorites, only: [:create, :destroy]
   resources :post_comments, only: [:create, :destroy]
 end
-
+resources :users, only: [:show, :edit, :update]
 
 
 
